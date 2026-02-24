b508151a01ae0d26ebe31766409d2db29711ec18
Amit Dhaka <amitdhakaer@gmail.com>
2026-02-24 01:41:25 -0800

    initial commit

---
+++ src/app/ping/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function PingPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 text-white">
+            <div className="relative flex flex-col items-center">
+                PONG
+            </div>
+        </main>
+    );
+}
