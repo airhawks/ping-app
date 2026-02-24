b1b72451ead1e8329726c4ffade860155cd3a240
Amit Dhaka <amitdhakaer@gmail.com>
2026-02-24 01:48:09 -0800

    Standardize indentation to 2 spaces and add .editorconfig

--- src/app/ping/page.tsx
+++ src/app/ping/page.tsx
@@ -1,11 +1,9 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function PingPage() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 text-white">
-            <div className="relative flex flex-col items-center">
-                PONG
-            </div>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-950 text-white">
+      <div className="relative flex flex-col items-center">PONG</div>
+    </main>
+  );
 }
